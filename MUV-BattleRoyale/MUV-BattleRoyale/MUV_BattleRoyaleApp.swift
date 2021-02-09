674912e672627ca308b11089164ac57324361682
Luis Pereira <lfbp@academy.cin.ufpe.br>
2021-02-09 09:32:34 -0300

    empty project

---
+++ MUV-BattleRoyale/MUV-BattleRoyale/MUV_BattleRoyaleApp.swift
@@ -0,0 +1,17 @@
+//
+//  MUV_BattleRoyaleApp.swift
+//  MUV-BattleRoyale
+//
+//  Created by Luis Pereira on 09/02/21.
+//
+
+import SwiftUI
+
+@main
+struct MUV_BattleRoyaleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
